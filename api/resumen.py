5bbdef20d7678735d18a74d86838b0fbc2be0c11
rodrigosyonet-commits <rodrigo.syonet@gmail.com>
2026-07-13 16:05:58 -0600

    Add files via upload

--- api/resumen.py
+++ api/resumen.py
@@ -1,1 +1,6 @@
-
+from http.server import BaseHTTPRequestHandler
+import json
+class handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    data=[{"concepto":"Tramites","presupuesto":200000,"utilizado":125000,"porcentaje":62.5,"estado":"NARANJA"}]
+    self.send_response(200); self.end_headers(); self.wfile.write(json.dumps(data).encode())
