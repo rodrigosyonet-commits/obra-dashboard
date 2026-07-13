419d5e2e06b9bf3d317e375073582a13b49ff0f3
rodrigosyonet-commits <rodrigo.syonet@gmail.com>
2026-07-13 17:50:23 -0600

    Update route.ts

--- app/api/presupuesto/route.ts
+++ app/api/presupuesto/route.ts
@@ -2,28 +2,26 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
 
-  const query = `
-SELECT
-    C.cliente,
-    C.razonSocial,
-    D.renglon,
-    D.producto,
-    D.cantidad,
-    D.descripcion,
-    D.costo,
-    D.valorUnitario,
-    D.subtotalPresupuestado,
-    C.mes
-FROM DbContrato AS C
-INNER JOIN DbContratoDet AS D
-ON D.empresa=C.empresa
-AND D.sucursal=C.sucursal
-AND D.folioContrato=C.folioContrato
-WHERE
-C.empresa=@empresa
-AND C.sucursal=@sucursal
-TAMPAG 500
-`;
+  const query =
+  "SELECT " +
+  "C.cliente," +
+  "C.razonSocial," +
+  "D.renglon," +
+  "D.producto," +
+  "D.cantidad," +
+  "D.descripcion," +
+  "D.costo," +
+  "D.valorUnitario," +
+  "D.subtotalPresupuestado," +
+  "C.mes " +
+  "FROM DbContrato AS C " +
+  "INNER JOIN DbContratoDet AS D " +
+  "ON D.empresa=C.empresa " +
+  "AND D.sucursal=C.sucursal " +
+  "AND D.folioContrato=C.folioContrato " +
+  "WHERE C.empresa=@empresa " +
+  "AND C.sucursal=@sucursal " +
+  "TAMPAG 500";
 
   const params = new URLSearchParams({
     tipo: "3",
